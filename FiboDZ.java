2372e8aa68910c8d5a077d54e8cd809c6e0d8222
kloooc <82811877+kloooc@users.noreply.github.com>
2021-11-27 14:16:47 +0100

    Create FiboDZ.java

---
+++ FiboDZ.java
@@ -0,0 +1,14 @@
+public class FiboDZ {
+    public static void main(String[] args) {
+        final int N = 5; //n-ty wyraz ciagu fibonaciego
+        System.out.println(fibo(N));
+
+    }
+    static int fibo(int n){
+        if (n==1 || n==2){
+            return 1;
+        } else {
+            return fibo(n-1) + fibo(n-2);
+        }
+    }
+}
